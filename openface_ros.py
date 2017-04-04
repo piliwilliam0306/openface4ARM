2e806c7964ebba6b3c43ae24372c7ee182508953
piliwilliam0306 <piliwilliam0306@gmail.com>
2017-04-04 23:43:36 +0800

    add tracking in openface_ros noe

--- openface_ros.py
+++ openface_ros.py
@@ -19,20 +19,15 @@
 # limitations under the License.
 
 import time
-
-start = time.time()
-
 import argparse
 import cv2
 import os
 import pickle
-
 from operator import itemgetter
-
 import numpy as np
 np.set_printoptions(precision=2)
 import pandas as pd
-
+import dlib
 import openface
 
 from sklearn.pipeline import Pipeline
@@ -50,7 +45,10 @@ from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import String, UInt16, Float64, Bool,UInt8
 from openface4ARM.srv import *
 bridge = CvBridge()
-count = 1
+count = 0
+trackingFace = 0
+rec_mode = False
+training_mode = False
 
 fileDir = os.path.dirname(os.path.realpath(__file__))
 modelDir = os.path.join(fileDir, 'models')
@@ -60,7 +58,7 @@ pickleDir = os.path.join(fileDir, 'data/mydataset/banana_feature')
 recDir = 'data/mydataset/banana_rec'
 featureDir = 'data/mydataset/banana_feature'
 path = ''
-images_required = 0.0
+images_required = 10.0
 transmit_progress = 0
 dlibFacePredictor = os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat")
 networkModel = os.path.join(openfaceModelDir, 'nn4.small2.v1.t7')
@@ -68,182 +66,151 @@ imgDim = 96
 
 def image_callback(msg):
     global count
-    #count = count + 1
+    global trackingFace
     banana = time.strftime("%H:%M:%S")
-    if count < images_required + 1:
-        rospy.loginfo("Received %s images!" %count)
-        try:
+    rgbImg = bridge.compressed_imgmsg_to_cv2(msg, "rgb8")
+    if rec_mode == True:
+    	infer(rgbImg)
+    if training_mode == True:	
+        if count < images_required:
+            try:
                 # Convert your ROS Image message to OpenCV2
-		start = time.time()
-		bgrImg = bridge.imgmsg_to_cv2(msg, "bgr8")
-		#bgrImg = bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
-		rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
-		outRgb = align.align(
-            	    imgDim,
-            	    rgbImg,
-                    #bb,
-            	    landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
-		if outRgb is not None:
-			#count = count + 1
-			outBgr = cv2.cvtColor(outRgb, cv2.COLOR_RGB2BGR)
-			print("face detect and align took {} seconds.".format(time.time() - start))
-			cv2.imwrite(os.path.join(path,'IMAGE%s.jpeg' %count), outBgr)
-			image_progress = count / images_required * 100
-                	pub.publish(image_progress)
-			# just for fun
-			faceImg = bridge.cv2_to_imgmsg(outBgr, "bgr8")
-			face_pub.publish(faceImg)	
-		else:
-			print("unable to find a face.")
-		    	return
-        except CvBridgeError, e:
+	        start = time.time()
+	        if not trackingFace:
+		    bb = align.getLargestFaceBoundingBox(rgbImg)
+		    if bb is not None:
+		        tracker.start_track(rgbImg,dlib.rectangle(bb.left(),bb.top(),bb.right(),bb.bottom()))
+		        rospy.loginfo("face detection took {} seconds.".format(time.time() - start))
+		        trackingFace = 1
+		    else:
+		        rospy.loginfo("unable to detect your face, please face the camera")
+	        else:
+		    trackingQuality = tracker.update(rgbImg)
+		    if trackingQuality >= 8.75:
+		        bb = tracker.get_position()
+		        bb = dlib.rectangle(int(bb.left()),int(bb.top()),int(bb.right()),int(bb.bottom()))
+		        outRgb = align.align(imgDim, rgbImg, bb,
+                	    landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+		        rospy.loginfo("face tracking and align took {} seconds.".format(time.time() - start))
+		        count = count + 1
+		        rospy.loginfo("Received {} images!".format(count))
+		        outBgr = cv2.cvtColor(outRgb, cv2.COLOR_RGB2BGR)
+		        #shape = predictor(rgbImg, bb)
+		        cv2.imwrite(os.path.join(path,'IMAGE%s.jpeg' %count), outBgr)
+		        image_progress = count / images_required * 100
+		        pub.publish(image_progress)
+		        faceImg = bridge.cv2_to_imgmsg(outBgr, "bgr8")
+		        face_pub.publish(faceImg)
+		    else:
+		        rospy.loginfo("fail to track, redetecting face")
+		        trackingFace = 0
+            except CvBridgeError, e:
                 print(e)
-    else:
-        return
 
-def getRep(imgPath):#, multiple=False):
+def getRep(rgbImg):
+    global trackingFace
     start = time.time()
-    bgrImg = cv2.imread(imgPath)
-    if bgrImg is None:
-        raise Exception("Unable to load image: {}".format(imgPath))
-
-    rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
-
-    #if args.verbose:
-    #    print("  + Original size: {}".format(rgbImg.shape))
-    #if args.verbose:
-    #    print("Loading the image took {} seconds.".format(time.time() - start))
-
-    #start = time.time()
-
-    #if multiple:
-    #    bbs = align.getAllFaceBoundingBoxes(rgbImg)
-    #else:
-    bb1 = align.getLargestFaceBoundingBox(rgbImg)
-    bbs = [bb1]
-    #if len(bbs) == 0 or (not multiple and bb1 is None):
-    #    raise Exception("Unable to find a face: {}".format(imgPath))
-    #if args.verbose:
-    #    print("Face detection took {} seconds.".format(time.time() - start))
-
-    reps = []
-    for bb in bbs:
-        start = time.time()
-        alignedFace = align.align(
-            imgDim,
-            rgbImg,
-            bb,
+    if not trackingFace:
+        # Get the largest face bounding box
+        bb = align.getLargestFaceBoundingBox(rgbImg) #Bounding box
+        if bb is not None:
+            tracker.start_track(rgbImg,dlib.rectangle(bb.left(),bb.top(),bb.right(),bb.bottom()))
+            trackingFace = 1
+        else:
+            motion_detected = False
+            rospy.loginfo("unable to detect your face, please face the camera")
+	    return []
+    else:
+        trackingQuality = tracker.update(rgbImg)
+        if trackingQuality >= 8.75:
+            bb = tracker.get_position()
+            bb = dlib.rectangle(int(bb.left()),int(bb.top()),int(bb.right()),int(bb.bottom()))
+        else:
+            rospy.loginfo("unable to detect your face, please face the camera")
+            trackingFace = 0
+            motion_detected = False
+            return []
+    rospy.loginfo("Face detection took {} seconds.".format(time.time() - start))
+    start = time.time()
+    alignedFace = align.align(imgDim, rgbImg, bb,
             landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
-        if alignedFace is None:
-            raise Exception("Unable to align image: {}".format(imgPath))
-        #if args.verbose:
-        #    print("Alignment took {} seconds.".format(time.time() - start))
-        #    print("This bbox is centered at {}, {}".format(bb.center().x, bb.center().y))
-
-        #start = time.time()
-	###comment out to ignore align face
-        rep = net.forward(alignedFace)
-        #rep = net.forward(rgbImg)
-        #if args.verbose:
-        #    print("Neural network forward pass took {} seconds.".format(
-        #        time.time() - start))
-        reps.append((bb.center().x, rep))
-
-    #rep = net.forward(rgbImg)
-    #reps.append((bb.center().x, rep))
-    sreps = sorted(reps, key=lambda x: x[0])
-    return sreps
+    if alignedFace is None:
+        return []
+    bgrImg = cv2.cvtColor(alignedFace, cv2.COLOR_RGB2BGR)
+    faceImg = bridge.cv2_to_imgmsg(bgrImg, "bgr8")
+    face_pub.publish(faceImg)
+    rospy.loginfo("Alignment took {} seconds.".format(time.time() - start))
+    start = time.time()
+    reps = []
+    reps.append(net.forward(alignedFace))
+    rospy.loginfo("Neural network forward pass took {} seconds.".format(time.time() - start))
+    return reps
 
 def train_callback(msg):
+    global training_mode
+    global rec_mode
+    training_mode = True
+    rec_mode = False
     global path
     global count 
-    global images_required
-    images_required = 60.0
-    count = 1
+    count = 0
     #path = ('data/mydataset/banana_aligned/{}/{}'.format(msg.data,msg.data))
-    path = ('data/mydataset/raw/{}'.format(msg.data))
+    path = ('data/mydataset/banana_aligned/{}'.format(msg.data))
     if not os.path.exists(path):
         os.makedirs(path)
 
-    #os.system('rm data/mydataset/banana_aligned/cache.t7')
+    os.system('rm data/mydataset/banana_aligned/cache.t7')
     while not rospy.is_shutdown():
     	if count == images_required:
-	    now = rospy.get_rostime()
-	    #os.system('./batch-represent/main.lua -outDir ./data/mydataset/banana_feature -data ./data/mydataset/banana_aligned')
+	    start = time.time()
+	    os.system('./batch-represent/main.lua -outDir ./data/mydataset/banana_feature -data ./data/mydataset/banana_aligned')
 	    #os.system('./batch-represent/main.lua -outDir ./data/mydataset/banana_feature -data ./data/mydataset/banana_aligned/{}'.format(msg.data))
 	    #test.lua is for register new member only
-	    os.system('./batch-represent/test.lua -outDir ./data/mydataset/banana_feature -data ./data/mydataset/banana_aligned/{}'.format(msg.data))
-	    new = rospy.get_rostime()
-	    diff = new.secs - now.secs
-	    rospy.loginfo("Feature generation took %i seconds", diff)
-	    print("Loading embeddings.")
-	    #fname = "{}/labels.csv".format(args.workDir)
+	    #os.system('./batch-represent/test.lua -outDir ./data/mydataset/banana_feature -data ./data/mydataset/banana_aligned/{}'.format(msg.data))
+	    rospy.loginfo("Feature generation took {} seconds".format(time.time()-start))
+	    rospy.loginfo("Loading embeddings.")
 	    fname = "{}/labels.csv".format(featureDir)
 	    labels = pd.read_csv(fname, header=None).as_matrix()[:, 1]
 	    labels = map(itemgetter(1),
 		         map(os.path.split,
 		             map(os.path.dirname, labels)))  # Get the directory.
-	    #fname = "{}/reps.csv".format(args.workDir)
 	    fname = "{}/reps.csv".format(featureDir)
 	    embeddings = pd.read_csv(fname, header=None).as_matrix()
 	    le = LabelEncoder().fit(labels)
 	    labelsNum = le.transform(labels)
 	    nClasses = len(le.classes_)
-	    print("Training for {} classes.".format(nClasses))
-
+	    rospy.loginfo("Training for {} classes.".format(nClasses))
 	    clf = SVC(C=1, kernel='linear', probability=True)
-
 	    clf.fit(embeddings, labelsNum)
-
-	    #fName = "{}/classifier.pkl".format(args.workDir)
 	    fName = "{}/classifier.pkl".format(featureDir)
-	    print("Saving classifier to '{}'".format(fName))
+	    rospy.loginfo("Saving classifier to '{}'".format(fName))
 	    with open(fName, 'w') as f:
 		pickle.dump((le, clf), f)
 	    pub1.publish(100)
 	    break
 
 def rec_callback(msg):
+    global rec_mode
+    global training_mode
+    training_mode == False
     if msg.data == True:
-        global path
-        global images_required
-        global count
-        path = 'data/mydataset/banana_rec'
-        images_required = 1.0
-        count = 0
-        #with open(args.classifierModel, 'r') as f:
-        with open(os.path.join(pickleDir,'classifier.pkl'), 'r') as f:
-        #f == os.path.join(pickleDir,'classifier.pkl')
-            (le, clf) = pickle.load(f)
-        while not rospy.is_shutdown():
-	    if count == images_required:
-	        #for img in recDir:
-	        img = "data/mydataset/banana_rec/IMAGE1.jpeg"
-		print("\n=== {} ===".format(img))
-		reps = getRep(img)#, multiple)
-		if len(reps) > 1:
-		    print("List of faces in image from left to right")
-		for r in reps:
-		    rep = r[1].reshape(1, -1)
-		    bbx = r[0]
-		    start = time.time()
-		    predictions = clf.predict_proba(rep).ravel()
-		    maxI = np.argmax(predictions)
-		    person = le.inverse_transform(maxI)
-		    confidence = predictions[maxI]
-		    #if args.verbose:
-		    #    print("Prediction took {} seconds.".format(time.time() - start))
-		    #if multiple:
-		    #    print("Predict {} @ x={} with {:.2f} confidence.".format(person, bbx,
-		    #                                                                 confidence))
-		    #else:
-		    print("Predict {} with {:.2f} confidence.".format(person, confidence))
-		    if isinstance(clf, GMM):
-		        dist = np.linalg.norm(rep - clf.means_[maxI])
-		        print("  + Distance from the mean: {}".format(dist))
-	    	    pub2.publish(person)
-	        break
+	rec_mode = True
+	rospy.loginfo('Starting face recognition mode')
+    else:
+    	rec_mode = False
+	rospy.loginfo('Stopping face recognition mode')
 
+def infer(img):
+        with open(os.path.join(pickleDir,'classifier.pkl'), 'r') as f:
+            (le, clf) = pickle.load(f)
+	reps = getRep(img)
+	for r in reps:
+	    rep = r.reshape(1, -1)
+	    predictions = clf.predict_proba(rep).ravel()
+	    maxI = np.argmax(predictions)
+	    person = le.inverse_transform(maxI)
+	    confidence = predictions[maxI]
+	    pub2.publish(person)
 
 if __name__ == '__main__':
     if not os.path.exists(recDir):
@@ -256,20 +223,13 @@ if __name__ == '__main__':
     train_topic = "cmdTraining"
     rec_topic = "cmdRecognition"
     image_topic = "croppedImages/compressed"
-    #rospy.Subscriber(image_topic, CompressedImage, image_callback)
-    rospy.Subscriber(image_topic, Image, image_callback)
+    rospy.Subscriber(image_topic, CompressedImage, image_callback)
     rospy.Subscriber(train_topic, String, train_callback)    
     rospy.Subscriber(rec_topic, Bool, rec_callback)
-
-    start = time.time()
-
     align = openface.AlignDlib(dlibFacePredictor)
-    net = openface.TorchNeuralNet(networkModel, imgDim=imgDim)#,
-    #                              cuda=args.cuda)
-    """	
-    if args.verbose:
-        print("Loading the dlib and OpenFace models took {} seconds.".format(
-            time.time() - start))
-        start = time.time()
-    """
+    net = openface.TorchNeuralNet(networkModel, imgDim=imgDim)
+    tracker = dlib.correlation_tracker()
+    predictor = dlib.shape_predictor(dlibFacePredictor)
+    #win = dlib.image_window()
+
     rospy.spin() 
